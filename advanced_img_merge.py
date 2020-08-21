a683ba6d4cc1a819572b6e0e6b997ed7c99acf6f
Kstyle0710 <jongbaekim0710@gmail.com>
2020-08-21 15:25:19 +0900

    adding function for texting on images

--- advanced_img_merge.py
+++ advanced_img_merge.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import glob
 import math
 
@@ -13,18 +13,26 @@ td_len = math.ceil(len(img_list)/2)
 print(td_len)
 
 w, h = 426, 240   # 이미지 리사이즈 가로, 세로 길이
+font = ImageFont.truetype('Roboto-Bold.ttf', size=150)
+color = 'rgb(255, 0, 0)'
+(x, y) = (15, 15)
 
 new_image = Image.new('RGB', (2 * w, td_len * h), (250, 250, 250))
 
 for idx, img in enumerate(img_list):
     print(idx)
     image = Image.open('image/{0}'.format(img))
+    draw = ImageDraw.Draw(image)
+    message = "Photo_No.{0}".format(int(idx) + 1)
+    draw.text((x, y), message, fill=color, font=font)
+
     image = image.resize((w, h))
-    image_size = image.size
     if int(idx) % 2 == 0:
         new_image.paste(image, (0, int(h * int(idx)/2)))
     else:
         new_image.paste(image, (w, int(h * math.floor(int(idx)/2))))
+
+
 
 new_image.save("merged_images/merged1.jpg", "JPEG")
 new_image.show()
